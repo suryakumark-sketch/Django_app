e8e75a642fcd0f0ba0b02594762d18dd1c719119
suryakumar <suryakumar.shadow@gmail.com>
2026-01-15 21:13:04 +0530

    Changed postgress to sqlite

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -87,13 +87,8 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aibotdb',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
